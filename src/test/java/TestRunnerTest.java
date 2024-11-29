81eae53491e05dd6b7c6941cfa5d07786caa078a
Sorin <miroiu.sorin0@gmail.com>
2024-11-29 13:56:48 +0200

    Added support for testng suite run

--- src/test/java/TestRunnerTest.java
+++ src/test/java/TestRunnerTest.java
@@ -1,7 +1,9 @@
 import io.cucumber.testng.CucumberOptions;
 import io.cucumber.testng.AbstractTestNGCucumberTests;
 import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
 
+@Test
 @CucumberOptions(
         features = "src/test/resources/features",  // Path to your feature files
         glue = {"steps", "hooks"},
@@ -9,7 +11,7 @@ import org.testng.annotations.DataProvider;
         tags = "@NegativeLogin or @PositiveLogin",
         plugin = {"pretty", "html:target/cucumber-reports"}  // Cucumber reporting
 )
-public class TestRunner extends AbstractTestNGCucumberTests {
+public class TestRunnerTest extends AbstractTestNGCucumberTests {
 
     @Override
     @DataProvider(parallel = true)
